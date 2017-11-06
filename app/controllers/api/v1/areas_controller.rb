b925576b2c19e8e107efd37b75deac85386946f7
Thakur Ballary <thakurballary@gmail.com>
2017-11-06 23:59:47 +0530

    Area CRUD methods added

--- app/controllers/api/v1/areas_controller.rb
+++ app/controllers/api/v1/areas_controller.rb
@@ -2,8 +2,35 @@ module Api
 	module V1
 		class AreasController < ApplicationController
 
+			before_action :set_area, only: [:update, :destroy]
+
 			def index
 				render json: Area.order(:position)
+			end
+
+			def create
+				area = Area.create(area_params)
+				render json: area
+			end
+
+			def update
+				@area.update_attributes(area_params)
+				render json: @area
+			end
+
+			def destroy
+				@area.destroy	
+				render json: @area			
+			end
+
+			private
+
+			def area_params
+				params.require(area).permit(:position, :name)
+			end
+
+			def set_area
+				@area = Area.find(params[:id])
 			end
 
 		end
